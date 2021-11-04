5a37e328f315794d1fe88866d55625236ccdeac4
NaumenkoSergiy <s.naumenko@active-bridge.com>
2021-11-04 16:21:50 +0200

    add additional fields for favorited_products table

--- db/migrate/20211104133036_create_favorited_products.rb
+++ db/migrate/20211104133036_create_favorited_products.rb
@@ -1,6 +1,8 @@
 class CreateFavoritedProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :favorited_products do |t|
+      t.references :product, null: false
+      t.references :user, null: false
 
       t.timestamps
     end
